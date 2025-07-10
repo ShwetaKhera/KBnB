2d8a3075100f4473fd204444dba3a1ac78f94dfc
Shweta Khera <shwetakhera96@gmail.com>
2025-07-10 16:59:53 +1000

    Update index.js

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ app.use(cookieParser())
 app.use('/uploads', express.static(__dirname + '/uploads'))
 app.use(cors({
     credentials: true,
-    origin: "https://kbnb.netlify.app/"
+    origin: "https://kbnb.netlify.app"
 }))
 
 mongoose.connect(process.env.MONGODB_CONNECTION_URL)
